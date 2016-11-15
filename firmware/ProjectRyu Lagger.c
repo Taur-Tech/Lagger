a232daf5e1aad7d39c4b5b5328e58db17a923006
Taur-Tech <andrei.chelariu@taur-tech.com>
2016-11-15 14:10:13 +0900

    put license text in comment

--- firmware/ProjectRyu Lagger.c
+++ firmware/ProjectRyu Lagger.c
@@ -1,4 +1,4 @@
-Copyright 2016 Stefan Andrei Chelariu
+/* Copyright 2016 Stefan Andrei Chelariu
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@ Copyright 2016 Stefan Andrei Chelariu
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-
+*/
 
 /**************************
 *Project Ryu Guitar Effect*
